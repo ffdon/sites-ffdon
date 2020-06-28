d6f756b122cc18ce83cc988b3136a1ae67e70d3d
ff-mgk <freifunk@growngizmo.de>
2020-06-28 12:25:03 +0200

    Update site.mk

--- site.mk
+++ site.mk
@@ -1,32 +1,53 @@
-GLUON_SITE_PACKAGES := \
-        gluon-alfred \
-        gluon-respondd\
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-ebtables-source-filter \
-	gluon-web-admin \
-	gluon-web-autoupdater \
-	gluon-web-network \
-	gluon-web-private-wifi \
-	gluon-web-wifi-config \
-	gluon-mesh-batman-adv-15 \
-	gluon-mesh-vpn-tunneldigger \
-	gluon-radvd \
-	gluon-setup-mode \
-	gluon-status-page \
-	haveged \
-	iptables \
-	gluon-mesh-vpn-core \
-	iwinfo \
-	gluon-ssid-changer \
-	gluon-rfkill-disable
+##	gluon site.mk makefile 
 
+##	GLUON_FEATURES
+#		Specify Gluon features/packages to enable;
+#		Gluon will automatically enable a set of packages
+#		depending on the combination of features listed
+
+GLUON_FEATURES := \
+	autoupdater \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	ebtables-limit-arp \
+	ebtables-source-filter \
+	mesh-batman-adv-15 \
+	mesh-vpn-tunneldigger \
+	radvd \
+	respondd \
+	status-page \
+	web-advanced \
+	web-wizard \
+        web-private-wifi \
+	advancedstats \
+        config-mode-statistics \
+	ssid-changer \
+	tunneldigger-watchdog \
+        rfkill-disable
+##	GLUON_SITE_PACKAGES
+#		Specify additional Gluon/OpenWrt packages to include here;
+#		A minus sign may be prepended to remove a packages from the
+#		selection that would be enabled by default or due to the
+#		chosen feature flags
+GLUON_SITE_PACKAGES := \
+	haveged \
+	iwinfo \
+	iptables \
+	gluon-alfred
+#GLUON_SITE_PACKAGES := \
+#        gluon-config-mode-autoupdater \
+#	gluon-config-mode-contact-info \
+#	gluon-config-mode-core \
+#	gluon-config-mode-geo-location \
+#	gluon-config-mode-hostname \
+#	gluon-web-admin \
+#	gluon-web-autoupdater \
+#	gluon-web-network \
+#	gluon-web-private-wifi \
+#	gluon-web-wifi-config \
+#	gluon-setup-mode \
+#	gluon-mesh-vpn-core \
+	
 GLUON_PRIORITY ?= 3
 GLUON_LANGS ?= de
 GLUON_ATH10K_MESH := ibss
